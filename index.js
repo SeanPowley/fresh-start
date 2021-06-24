ef0ee1daaae203339ea96b8046cc965099027398
Sean Powley <seanpowley@yahoo.com>
2021-06-24 19:50:44 -0400

    please work

---
+++ index.js
@@ -0,0 +1,91 @@
+// const name1 = 'rod'
+// name1 = 'sean'
+// console.log(name1)
+
+// const obj = {
+//     name: 'rod',
+//     color: 'red'
+// }
+
+// var is global
+// let/const are block scope
+
+// let arr = ['rod', 'sean', 'roy', 'rob']
+
+// const name1 = 'rodjlfsadjlkdsfjlkfdsjlkfsadjklfads'
+// console.log(name1[0].toUpperCase() + name1.slice(1))
+
+// console.log(name1[1])
+
+// let name1 = 'rod'
+//     // 'r', 'o', 'd' // arrays have built in methods like .length, .reverse, foreach, .map, .filter, etc
+
+// name1 = name1.split('')
+// name1 = name1.splice(0, 1, )
+// console.log(name1)
+// console.log(name1.split('').splice(0, 1))
+
+const arr = ["red", "blue", "green"];
+const arr2 = ["mike", "rod", "sean"];
+arr.splice(2, 2, "hhhh");
+console.log(arr, arr2);
+
+// while i = 0 and i < 3 increment i
+// arr[0] === 'rod'
+// arr[1] ==== 'sean'
+// arr[2] === 'roy'
+
+// i <= 4
+// 0 <= 4
+// 1 <= 4
+// 2 <= 4
+// 3 <= 4
+// 4 <= 4
+
+// for (let i = 0; i < arr.length; i = i + 1) {
+//     console.log(arr[i])
+
+// }
+
+// all of there html,css,js
+// server may compile a static page
+// static page gets sent to server
+// hey server theres this thing i clicked and i want to see
+// oh okay here it is
+// server goes and checks for a static page
+
+// function multiply(num1, num2) {
+//     return (num1 * num2)
+// }
+
+// const functionValue = multiply(5, 10)
+
+// console.log(multiply(5, 10))
+
+// console.log('this is functionvalue,', functionValue)
+
+// let first = 5
+// let second = 10
+
+// function secondFunction(num) {
+//     return multiply(first, second) * num
+
+//     // undefined * 5
+// }
+
+// hey console the value of these is 50
+// but return is binding the value 50 to the function
+// console.log(secondFunction(5))
+
+// nameCaller('rod', 'sean', 'roy')
+
+// function converter(num) {
+
+// }
+
+// converter('rod')
+
+// arrays have built in methods
+// 1. is .length
+
+// console.log(arr[0])
